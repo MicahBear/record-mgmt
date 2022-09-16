9f65dbd4ba9ebe215539fb632e52220bac6c26b0
MicahBear <micah.b.elm@gmail.com>
2022-09-16 16:55:52 -0700

    fix connection to db/set controller for landing page

---
+++ routes/main.js
@@ -0,0 +1,11 @@
+// will handle login routing
+
+const express = require("express");
+const router = express.Router();
+const homeController = require("../controllers/home");
+
+//Main Routes - simplified for now
+router.get("/", homeController.getIndex);
+
+
+module.exports = router;
