a0291e6cdb657f1a0f38889c765c0a2d1eb961d1
MicahBear <micah.b.elm@gmail.com>
2022-09-23 17:29:30 -0700

    styling

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,7 @@
 module.exports = {
     getIndex: async (req, res) => {
         try {
-            console.log('testing')
+            console.log('landing page rendered.')
             res.render('index.ejs')
         } catch (error) {
             console.log(error)
