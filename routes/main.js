6615336c6b9a2014119879780f29a492930849b0
MicahBear <micah.b.elm@gmail.com>
2022-09-25 13:40:33 -0700

    routes for auth state/comment

--- routes/main.js
+++ routes/main.js
@@ -3,8 +3,19 @@ const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/home");
 
-// controller > home > getIndex
+// render landing page @controllers>home
 router.get('/', homeController.getIndex);
+//render login page @controllers>authState
+router.get('/login',);
+//reach database for login @controllers>authState
+router.post('/login',);
+//reach server to terminate login @controllers>authState
+router.get('/logout',);
+// render signup page @controllers>authState
+router.get('/signup',);
+// reach database to initialize a new user
+router.post('signup',);
+
 
 
 module.exports = router;
