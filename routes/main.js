78ab47e17acf7e31a049c7507fa79f43fbb542bb
MicahBear <micah.b.elm@gmail.com>
2022-09-25 13:02:54 -0700

    labeling

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/home");
 
-//Main Routes - simplified for now
+// controller > home > getIndex
 router.get('/', homeController.getIndex);
 
 
