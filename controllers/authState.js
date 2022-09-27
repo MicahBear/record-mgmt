55441fddc67bcafd18bd73dd2a6cda85301bbdfe
MicahBear <micah.b.elm@gmail.com>
2022-09-27 12:46:42 -0700

    create route to dashboard for user

--- controllers/authState.js
+++ controllers/authState.js
@@ -1,17 +1,48 @@
-// const passport = require("passport");
-// const validator = require("validator");
-// const User = require("../models/User");
+const passport = require("passport");
+const validator = require("validator");
+const User = require("../models/User");
 
-exports.getLogin = (request, response) => {
+exports.getLogin = (req, res) => {
     try {
         console.log('login page rendered.')
-        response.render('login.ejs')
+        res.render('login.ejs')
     } catch (error) {
         console.error(error)
     }
 };
 
-// exports.postLogin = () => { };
+exports.postLogin = (req, res, nex) => {
+    const validationErrors = [];
+    if (!validator.isEmail(req.body.email))
+        validationErrors.push({ msg: "Please enter a valid email address." });
+    if (validator.isEmpty(req.body.password))
+        validationErrors.push({ msg: "Password cannot be blank." });
+
+    if (validationErrors.length) {
+        req.flash("errors", validationErrors);
+        return res.redirect("/login");
+    }
+    req.body.email = validator.normalizeEmail(req.body.email, {
+        gmail_remove_dots: false,
+    });
+
+    passport.authenticate("local", (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            req.flash("errors", info);
+            return res.redirect("/login");
+        }
+        req.logIn(user, (err) => {
+            if (err) {
+                return next(err);
+            }
+            req.flash("success", { msg: "Success! You are logged in." });
+            res.redirect(req.session.returnTo || "/profile");
+        });
+    })(req, res, next);
+};
 
 // exports.getLogout = () => { };
 
--- routes/main.js
+++ routes/main.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 const homeController = require("../controllers/home");
+const { ensureAuth } = require("../middleware/auth");
 
 // render landing page @controllers>home
 router.get('/', homeController.getIndex);
-
+// render dashboard and ensure auth
+router.get('/dashboard', ensureAuth, dashController.getDashboard);
 
 
 module.exports = router;
