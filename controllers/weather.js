6365e1bbeb7819e0a325f5c9eacbbcd549d9b4ce
MicahBear <micah.b.elm@gmail.com>
2022-09-30 11:22:09 -0700

    update css outline index.ejs/update controller

--- controllers/weather.js
+++ controllers/weather.js
@@ -2,17 +2,19 @@ const fetch = require('node-fetch');
 require('dotenv').config({ path: '../config/.env' })
 // const User = require('../models/User');
 
-module.exports = {
-    postWeather: async (req, res) => {
-        const apiKey = `${process.env.WEATHER_API}`;
-        try {
-            let city = req.body.city;
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-            const response = await fetch(url)
-            const body = await response.json()
-            console.log(body)
-        } catch (error) {
-            console.log(error);
-        }
+exports.postWeather = async (req, res) => {
+    const apiKey = `${process.env.WEATHER_API}`;
+    try {
+        let city = req.body.city;
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+        const response = await fetch(url)
+        const body = await response.json()
+        console.log(body)
+    } catch (error) {
+        console.log(error);
     }
-};
+}
+
+
+
+
